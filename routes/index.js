a9199ec1d5d23d1d9a686a81ad667cf97999e773
nodester <nodester@localhost.localdomain>
2014-11-14 15:13:19 -0800

    first api

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router();
 var cassandra = require('cassandra-driver');
 var client = new cassandra.Client({ contactPoints: ['127.0.0.1'], keyspace: 'myk'});
 
+var client2 = new cassandra.Client({ contactPoints: ['sv2lxdprep01.corp.equinix.com', 'sv2lxdprep02.corp.equinix.com', 'sv2lxdprep04.corp.equinix.com', 'sv2lxdprep05.corp.equinix.com'], keyspace: 'BCM_KEY_SPACE'});
+
 var redis  = require("redis");
 var redisClient = redis.createClient(6379, '127.0.0.1');
 
@@ -32,6 +34,103 @@ router.get('/hello', function(req, res) {
 });
 
 /* GET Userlist page. */
+router.get('/api/CustomerCageCabinet/:timePeriod/:customer/:cage/:cabinet', function(req, res) {
+    var timePeriod =  req.params.timePeriod;
+    var ttimePeriod = "";
+    var customer =  req.params.customer;
+    var cage =  req.params.cage;
+    var cabinet =  req.params.cabinet;
+    switch (timePeriod.toLowerCase()) {
+        case "hourly":
+            ttimePeriod = "CUSTOMER_CABINET_HOURLY_DATA";
+            break;
+        case "daily":
+            ttimePeriod = "CUSTOMER_CABINET_DAILY_DATA";
+            break;
+        default:
+            ttimePeriod = undefined;
+    }
+    
+    if (!(ttimePeriod && customer && cage && cabinet)) {
+        return renderResults({rows:[]});
+    }
+
+    var query = 'select * from "' + ttimePeriod + '"';
+    query += " where key= '2014' ";
+    query += "and key2 = '" + customer + "' ";
+    query += "and key3 = 'TY2' ";
+    query += "and key4 = '" + cage + "' ";
+    query += "and key5 = '" + cabinet + "' ";
+    query += "and column1 >= '20140101' and column1 <= '20141231';";
+    var key = sha1(query);
+    redisClient.select(0);
+    redisClient.get(key, function(err, cachedValue) {
+        if (cachedValue) {
+            redisClient.expire(key, 10);
+            renderResults(JSON.parse(cachedValue));
+            console.log("Cached");
+        }
+        else {
+            client2.execute(query, [], function(err, result) {
+                renderResults(result);
+                console.log("Not Cached");
+            });
+        }
+    })
+
+    function renderResults(resultIn) {
+        var result = { rawCount: resultIn.rows.length, aggregatedCount: 0, rows: [] };
+        delete result['meta'];
+        var row = {};
+        resultIn.rows.forEach(function(obj) {
+            if (obj.column1 !== row.timestamp) {
+                if (row.timestamp) {
+                    result.rows.push(row);
+                    result.aggregatedCount++;
+                }
+                row = initRow(obj, row);
+            }
+            else {
+                row = addMeasurement(obj, row);
+            }
+        });
+        function initRow(obj, row) {
+            row = {};
+            row.customerID = obj.key2;
+            row.cageID = obj.key4;
+            row.cabinetID = obj.key5;
+            row.timestamp = obj.column1;
+            row = addMeasurement(obj, row);
+            return row;
+        }
+        function addMeasurement(obj, row) {
+            row[obj.column5] = obj.value;
+            return row;
+        }
+        if (row.timestamp) {
+            result.rows.push(row);
+            result.aggregatedCount++;
+        }
+        res.write(JSON.stringify(result));
+        res.end();
+    }
+});
+
+/* GET Userlist page. */
+router.get('/equinix', function(req, res) {
+    renderResults({rows: []});
+
+    function renderResults(result) {
+//        console.log(result);
+        result.rows.forEach(function(obj) {
+            delete obj['__columns'];
+        });
+        res.write(JSON.stringify(result));
+        res.end();
+    }
+});
+
+/* GET Userlist page. */
 router.get('/cassandra', function(req, res) {
     var query = 'SELECT * FROM users';
     var key = sha1(query);
@@ -40,17 +139,19 @@ router.get('/cassandra', function(req, res) {
         if (cachedValue) {
             redisClient.expire(key, 10);
             renderResults(JSON.parse(cachedValue));
+            console.log("Cached");
         }
         else {
             client.execute(query, [], function(err, result) {
                 var user1 = redisClient.hgetall('user:1', function(err, user) {
                     if (user) {
-                        var newUser = {"username": user.name, "email": user.email};
+                        var newUser = {"username": user.username, "email": user.email};
                         result.rows.push(newUser);
                     }
                     redisClient.set(key, JSON.stringify(result));
                     redisClient.expire(key, 10);
                     renderResults(result);
+                    console.log("Not Cached");
                 });
             });
         }
@@ -60,7 +161,7 @@ router.get('/cassandra', function(req, res) {
         res.render('userlist', {
             "userlist": result.rows
         });
-        console.log(result);
+//        console.log(result);
     }
 });
 
